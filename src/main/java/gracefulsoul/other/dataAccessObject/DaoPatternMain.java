4df191b79b25193ce4e91adabf022f8272e5462e
yongsik <mymyyoo@naver.com>
2021-03-27 13:24:32 +0900

    Add codes about Data Access Object Pattern

--- src/main/java/gracefulsoul/other/dataAccessObject/DaoPatternMain.java
+++ src/main/java/gracefulsoul/other/dataAccessObject/DaoPatternMain.java
@@ -1,23 +1,23 @@
 package gracefulsoul.other.dataAccessObject;
 
 public class DaoPatternMain {
-    
-    public static void main(String[] args) {
-        StudentDao studentDao = new StudentDaoImpl();
-        
-        // print all students
-        for (Student student : studentDao.getAllStudents()) {
-            System.out.println("Student: [RollNo : " + student.getRollNo() + ", Name : " + student.getName() + " ]");
-        }
-        
-        // update student
-        Student student = studentDao.getAllStudents().get(0);
-        student.setName("Michael");
-        studentDao.updateStudent(student);
-        
-        // get the student
-        studentDao.getStudent(0);
-        System.out.println("Student: [RollNo : " + student.getRollNo() + ", Name : " + student.getName() + " ]");
-    }
-    
+
+	public static void main(String[] args) {
+		StudentDao studentDao = new StudentDaoImpl();
+
+		// Print all students.
+		for (Student student : studentDao.getAllStudents()) {
+			System.out.println("Student: [RollNo : " + student.getRollNo() + ", Name : " + student.getName() + " ]");
+		}
+
+		// Update student.
+		Student student = studentDao.getAllStudents().get(0);
+		student.setName("Michael");
+		studentDao.updateStudent(student);
+
+		// Get the student.
+		studentDao.getStudent(0);
+		System.out.println("Student: [RollNo : " + student.getRollNo() + ", Name : " + student.getName() + " ]");
+	}
+
 }
