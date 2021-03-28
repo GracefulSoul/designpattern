f23d5c4ad999e88b8c6d5b59a70cff724145ccb2
yongsik <mymyyoo@naver.com>
2021-03-28 15:17:04 +0900

    Add codes about Design Patterns

--- src/main/java/gracefulsoul/other/compositeEntity/CompositeEntityPatternMain.java
+++ src/main/java/gracefulsoul/other/compositeEntity/CompositeEntityPatternMain.java
@@ -6,6 +6,7 @@ public class CompositeEntityPatternMain {
 		Client client = new Client();
 		client.setData("Test", "Data");
 		client.printData();
+		
 		client.setData("Second Test", "Data1");
 		client.printData();
 	}
