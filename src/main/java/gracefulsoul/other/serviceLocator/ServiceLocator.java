e9774569c0c399c188b026e13de8b4321efde808
yongsik <mymyyoo@naver.com>
2021-03-28 15:11:44 +0900

    Add codes about Service Locator Pattern

--- src/main/java/gracefulsoul/other/serviceLocator/ServiceLocator.java
+++ src/main/java/gracefulsoul/other/serviceLocator/ServiceLocator.java
@@ -5,16 +5,17 @@ import gracefulsoul.other.serviceLocator.service.Service;
 public class ServiceLocator {
 
 	private static Cache cache = new Cache();
+	private static InitialContext context = new InitialContext();
 
 	public static Service getService(String jndiName) {
 		Service service = cache.getService(jndiName);
 		if (service != null) {
 			return service;
+		} else {
+			Service service1 = (Service) context.lookup(jndiName);
+			cache.addService(service1);
+			return service1;
 		}
-		InitialContext context = new InitialContext();
-		Service service1 = (Service) context.lookup(jndiName);
-		cache.addService(service1);
-		return service1;
 	}
 
 }
